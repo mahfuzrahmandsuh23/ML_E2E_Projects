091280750567d099311d6dc2782d2733792edb7f
Muhammad mahfuzur Rahman <m.mahfuzurrahamndsuh23@gmail.com>
2023-04-01 21:14:15 +0100

    logging and exception

---
+++ scrapping/GRevscrp.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import ElementNotVisibleException
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
+from bs4 import BeautifulSoup
+
+driver=webdriver.Chrome(ChromeDriverManager().install())
+
+url='https://www.google.com/maps/place/European+IT+Institute'
+driver.get(url)
+
