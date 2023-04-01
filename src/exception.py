edac96725bc54932e5708ad4cfdb44ececccc11b
Muhammad mahfuzur Rahman <m.mahfuzurrahamndsuh23@gmail.com>
2023-04-01 21:22:57 +0100

    updated exception file

--- src/exception.py
+++ src/exception.py
@@ -19,13 +19,13 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
 
-if __name__=="__main__":
+# if __name__=="__main__":
     
-    try:
-        a=1/0
+#     try:
+#         a=1/0
 
-    except Exception as e:
-        logging.info("divide by zero")
-        raise CustomException(e,sys)
-        
+#     except Exception as e:
+#         logging.info("divide by zero")
+#         raise CustomException(e,sys)
+     
        
